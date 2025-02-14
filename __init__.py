7d72400af600e1b13ffe11f7ac44865b4f1d6a9f
achraf079 <mester.saber01@gmail.com>
2025-02-14 13:51:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 from flask import json
 import sqlite3
@@ -9,14 +9,17 @@ app = Flask(__name__)
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
-@app.route('/somme/<int:valeur1>/<int:valeur2>')
-def somme(valeur1, valeur2):
-  resultat = valeur1 + valeur2
+@app.route('/somme')
+def somme():
+  valeurs = request.args.getlist('valeur')  #
+    total = 0
+    for valeur in valeurs:
+        total += int(valeur)
   if resultat % 2 == 0:
         parite = "pair"
   else:
         parite = "impair"
-  return f"<h2>La somme de {valeur1} et {valeur2} est : {resultat} ==> est donc {parite}</h2>"
+  return f"<h2>La somme des valeurs : {valeurs} est : {total} ==> est donc {parite}</h2>"
                                                                                                                                        
 @app.route('/')
 def hello_world():
