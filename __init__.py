60b8e9937cecda3346eff978653c422744994e52
achraf079 <mester.saber01@gmail.com>
2025-02-26 10:56:23 +0100

    update 2

--- __init__.py
+++ __init__.py
@@ -35,6 +35,10 @@ def exercices():
 def cnam():
     return render_template('cnam.html')
 
+@app.route('/Exercices HTML 2/')
+def Exercices_HTML():
+    return render_template('Exercices_HTML.html')
+
 @app.route('/cv/')
 def cv():
     return render_template('cv.html')
