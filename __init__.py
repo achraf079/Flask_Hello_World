aff88dd1db103ede711d519d28b576e86835cd94
achraf079 <mester.saber01@gmail.com>
2025-03-10 10:45:44 +0100

    sz

--- __init__.py
+++ __init__.py
@@ -27,54 +27,6 @@ def somme(valeurs):
 def hello_world():
     return "<h2>Bonjour tout le monde !</h2><p>Pour accéder à vos exerices cliquez <a href='./exercices/'>Ici</a></p>"
 
-@app.route('/exercices/')
-def exercices():
-    return render_template('exercices.html')
-  
-@app.route('/cnam/')
-def cnam():
-    return render_template('cnam.html')
-
-@app.route('/Exercices_HTML_2/')
-def Exercices_HTML():
-    return render_template('Exercices_HTML.html')
-
-@app.route('/tp_html/')
-def tp_html():
-    return render_template('tp_html.html')
-
-@app.route('/cv/')
-def cv():
-    return render_template('cv.html')
-
-@app.route('/formulaire.html/')
-def formulaire_html():
-    return render_template('formulaire.html')
-
-@app.route('/exercice_base3/')
-def exercice_base3():
-    return render_template('exercice_base3.html')
-
-@app.route('/exercice_base/')
-def liste():
-    return render_template('1_Liste_Base.html')
-
-@app.route('/maison/')
-def maison():
-    return render_template('Exemple_Base_SVG.html')
-
-@app.route('/vallet/')
-def svg_cards():
-    return render_template('svg_cards.html')
-
-@app.route('/chenille/')
-def svg_chenille():
-    return render_template('chenille.html')
-
-@app.route('/carre/')
-def carre():
-    return render_template('CSS_Carre.html')
-
 @app.route('/etoiles/')
 def etoiles():
     return render_template('carre_etoiles.html')
